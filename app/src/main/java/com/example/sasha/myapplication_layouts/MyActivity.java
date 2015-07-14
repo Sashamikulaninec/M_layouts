51cc43a6d31ecea9c65fc09972aa40bae6850d19
sasha <sashamikulaninec@yandex.ru>
2015-07-14 10:58:46 +0300

    first commit

--- app/src/main/java/com/example/sasha/myapplication_layouts/MyActivity.java
+++ app/src/main/java/com/example/sasha/myapplication_layouts/MyActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -18,13 +16,20 @@ import android.widget.Toast;
 
 public class MyActivity extends Activity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
-    private EditText m_eText_A;
-    private EditText m_eText_Op;
-    private EditText m_eText_B;
-    private Button m_btn_Res;
-    private TextView m_tView_Res;
-    private Switch sw_btn;
+    private EditText et_number_A;
+    private EditText et_operator;
+    private EditText et_number_B;
+    private Button btn_Result;
+    private TextView tv_Result;
+    private Switch sw_SwitchLayout;
     private LinearLayout linearLayout;
+    int num1 = 0;
+    int num2 = 0;
+    int res = 0;
+    public static final String ADD = "+";
+    public static final String SUBTRACT = "-";
+    public static final String MULTIPLE = "*";
+    public static final String DIVISION = "/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,61 +39,57 @@ public class MyActivity extends Activity implements View.OnClickListener, Compou
 
     }
 
-    private void findViews(){
-        m_eText_A   = (EditText) findViewById(R.id.et1_AM);
-        m_eText_Op  = (EditText) findViewById(R.id.et2_AM);
-        m_eText_B   = (EditText) findViewById(R.id.et3_AM);
-        m_btn_Res   = (Button) findViewById(R.id.btn_AM);
-        m_tView_Res = (TextView) findViewById(R.id.tv_AM);
-        sw_btn      = (Switch) findViewById(R.id.sw_AM);
+    private void findViews() {
+        et_number_A = (EditText) findViewById(R.id.et1_AM);
+        et_operator = (EditText) findViewById(R.id.et2_AM);
+        et_number_B = (EditText) findViewById(R.id.et3_AM);
+        btn_Result = (Button) findViewById(R.id.btn_AM);
+        tv_Result = (TextView) findViewById(R.id.tv_AM);
+        sw_SwitchLayout = (Switch) findViewById(R.id.sw_AM);
         linearLayout = (LinearLayout) findViewById(R.id.layout_activity);
 
-        sw_btn.setChecked(true);
-        sw_btn.setOnCheckedChangeListener(this);
-        m_btn_Res.setOnClickListener(this);
+        sw_SwitchLayout.setChecked(true);
+        sw_SwitchLayout.setOnCheckedChangeListener(this);
+        btn_Result.setOnClickListener(this);
 
     }
 
+
     @Override
-    public void onClick(View v){
-      int num1 = 0;
-      int num2 = 0;
-      int res  = 0;
-      if (TextUtils.isEmpty(m_eText_A.getText().toString()) || TextUtils.isEmpty(m_eText_B.getText().toString())) {
-      return;
-       }
-        num1 = Integer.parseInt(m_eText_A.getText().toString());
-        num2 = Integer.parseInt(m_eText_A.getText().toString());
+    public void onClick(View v) {
 
-        switch (v.getId()){
-            case R.id.btn_AM:
-                        if (m_eText_Op.getText().toString().equals("+")){
-                            res = num1 + num2;
-                            m_tView_Res.setText(" " +res);
+        if (TextUtils.isEmpty(et_number_A.getText().toString()) || TextUtils.isEmpty(et_number_B.getText().toString())) {
+            Toast.makeText(getApplication(), "Please enter number", Toast.LENGTH_SHORT).show();
+        } else {
+            num1 = Integer.parseInt(et_number_A.getText().toString());
+            num2 = Integer.parseInt(et_number_B.getText().toString());
 
-                        }
-                else
-                        if (m_eText_Op.getText().toString().equals("-")) {
-                            res = num1 - num2;
-                            m_tView_Res.setText(" " + res);
-                        }
-                else
-                        if (m_eText_Op.getText().toString().equals("*")) {
-                            res = num1 * num2;
-                            m_tView_Res.setText(" " + res);
-                        }
-                else
-                        if (m_eText_Op.getText().toString().equals("/")) {
-                            res = num1 / num2;
-                            m_tView_Res.setText(" " + res);
-                        }
+            try {
 
-            break;
 
+                String s = et_operator.getText().toString();
+                if (s.equals(ADD)) {
+                    res = num1 + num2;
+                    tv_Result.setText(String.valueOf(res));
+
+                } else if (s.equals(SUBTRACT)) {
+                    res = num1 - num2;
+                    tv_Result.setText(String.valueOf(res));
+
+                } else if (s.equals(MULTIPLE)) {
+                    res = num1 * num2;
+                    tv_Result.setText(String.valueOf(res));
+
+                } else if (s.equals(DIVISION)) {
+                    res = num1 / num2;
+                    tv_Result.setText(String.valueOf(res));
+
+                }
+            }
+            catch (ArithmeticException e){
+                Toast.makeText(this, "На нуль ділити не можна!", Toast.LENGTH_LONG).show();
+            }
         }
-
-
-
     }
 
     @Override
@@ -101,6 +102,5 @@ public class MyActivity extends Activity implements View.OnClickListener, Compou
 
     }
 }
-
 
 
